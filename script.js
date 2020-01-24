40145915c6d36b599f08ea3a049648db311ca464
abrakhan916 <abrar.khan916@gmail.com>
2020-01-24 11:46:08 +0000

    First stats added to game

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const diceImage = document.getElementById("diceImage");
 const totalScore = document.getElementById("totalScore");
 const gameMessage = document.getElementById("gameMessage");
 const restartButton = document.getElementById("restartButton");
+const timesWon = document.getElementById("timesWon");
+const timesLost = document.getElementById("timesLost");
 // consts go here
 
 // restartButton.style.visibility = "hidden" // if you don't want button visible
@@ -14,11 +16,14 @@ gameMessage.style.visibility = "hidden";
 let numberRolled;
 let pointsScored = 0;
 
+let numOfWins = [];
+let numOfLoses = [];
+
 // function declaration
 
 const diceRoll = () => {
     numberRolled = Math.ceil(Math.random() * 6);
-    console.log(`Roll ceil variable ${numberRolled}`);
+    // console.log(`Roll ceil variable ${numberRolled}`);
     gameMessage.style.visibility = "visible"
 }
 
@@ -27,24 +32,25 @@ const winOrLose = () => {
         gameMessage.textContent = "Game over. You lose.";
         rollButton.style.visibility = "hidden";
         restartButton.style.visibility = "visible"
+        // num of loses
+        numOfLoses.push(1);
+        console.log(numOfLoses);
+        // console.log(`Number of times lost: ${numOfLoses.length}`)
+        timesLost.textContent = `Number of times lost: ${numOfLoses.length}`;
     } else if(pointsScored > 20) {
         gameMessage.textContent = "You win. Well Done.";
         rollButton.style.visibility = "hidden";
         restartButton.style.visibility = "visible"
+        //num of wins
+        numOfWins.push(1);
+        console.log(numOfWins)
+        // console.log(`Number of times won: ${numOfWins.length}`)
+        timesWon.textContent = `Number of times won: ${numOfWins.length}`;
     } else {
         gameMessage.textContent = "Roll again";
     }
 }
 
-
-
-// const scoreAddition = () => {
-//     diceRoll()
-//     pointsScored += numberRolled
-//     console.log(`My total score ${pointsScored}`);
-// }
-
-// scoreAddition()
 
 rollButton.addEventListener("click", () =>{
     diceRoll();
